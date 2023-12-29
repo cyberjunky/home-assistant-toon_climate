64c8dfe811c22caf3c8a0aa3a09c2dbfbafb9204
Ron <ron@cyberjunky.nl>
2023-12-29 09:44:16 +0100

    Create const.py

---
+++ custom_components/toon_climate/const.py
@@ -0,0 +1,9 @@
+"""Constants for the Toon Thermostat."""
+
+DOMAIN = "climate"
+
+ACTIVE_STATE_AWAY = 3
+ACTIVE_STATE_COMFORT = 0
+ACTIVE_STATE_HOLIDAY = 4
+ACTIVE_STATE_HOME = 1
+ACTIVE_STATE_SLEEP = 2
